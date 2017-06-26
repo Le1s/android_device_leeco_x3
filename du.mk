867a1cd6008807361f0a4842595ca2e2c59b2548
Sahil Soni <sscsps@gmail.com>
2017-06-26 22:31:04 +0530

    Few Re-Arangements
    removed 32bit Processor configs

--- du.mk
+++ du.mk
@@ -1,8 +1,11 @@
 # Inherit device configuration
-$(call inherit-product, $(LOCAL_PATH)/device.mk)
+$(call inherit-product, device/leeco/x3/device.mk)
 
 # Inherit some common DU stuff.
 $(call inherit-product, vendor/du/config/common_full_phone.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := x3
 
 # Device display
 TARGET_SCREEN_HEIGHT := 1920
@@ -11,8 +14,7 @@ TARGET_SCREEN_WIDTH := 1080
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := x3
 PRODUCT_NAME := du_x3
-PRODUCT_BRAND := leeco
-PRODUCT_MODEL := leeco x3
-PRODUCT_MANUFACTURER := leeco
-PRODUCT_RELEASE_NAME := x3
+PRODUCT_BRAND := LeEco
+PRODUCT_MODEL := LeEco Le 1s
+PRODUCT_MANUFACTURER := LeEco
 PRODUCT_RESTRICT_VENDOR_FILES := false
