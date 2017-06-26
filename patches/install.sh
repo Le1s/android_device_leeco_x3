7c995ea6eabe7f836ab26b00f0e9495ccfed780d
Sahil Soni <sscsps@gmail.com>
2017-06-26 22:49:28 +0530

    removed system_netd_0001.patch
    This was for wifi hotspot fix. but it doesn't work and doesn't ven apply. so removed it.

--- patches/install.sh
+++ patches/install.sh
@@ -2,7 +2,7 @@ echo $1
 rootdirectory="$PWD"
 # ---------------------------------
 
-dirs="bionic frameworks/av frameworks/base packages/apps/Settings system/core system/netd"
+dirs="bionic frameworks/av frameworks/base packages/apps/Settings system/core"
 
 for dir in $dirs ; do
 	cd $rootdirectory
