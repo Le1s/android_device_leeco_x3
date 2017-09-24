03a56912bde12a577659d9dd95efc46d4171dba7
Sahil Soni <sscsps@gmail.com>
2017-09-25 03:43:54 +0530

    Add hwcomposer.mt6795.1.4

---
+++ libs/hwcomposer.mt6795.1.4/Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := hwcomposer.mt6795.1.4
+LOCAL_SRC_FILES_64 := hwcomposer.mt6795.1.4.a
+LOCAL_SRC_FILES_32 := arm/hwcomposer.mt6795.1.4.a
+LOCAL_MULTILIB := both
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+LOCAL_MODULE_SUFFIX := .a
+include $(BUILD_PREBUILT)
