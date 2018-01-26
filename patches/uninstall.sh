6050f0f976fb4edbcc5f14807886ce029222e584
Sahil Soni <sscsps@gmail.com>
2018-01-27 04:02:34 +0530

    Update patches for O
    Remove those that cant be applied.
    Modified that are needed(more are probably needed, but lets see with these for now).
    
    Signed-off-by: Sahil Soni <SscSPs@gmail.com>

--- patches/uninstall.sh
+++ patches/uninstall.sh
@@ -2,7 +2,7 @@ echo $1
 rootdirectory="$PWD"
 # ---------------------------------
 
-dirs="bionic frameworks/av frameworks/base system/core system/netd"
+dirs="frameworks/base system/core"
 
 for dir in $dirs ; do
 	cd $rootdirectory
