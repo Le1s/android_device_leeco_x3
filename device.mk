70ba3f82b9d734f698586a89ea249c29cfe09520
Sahil Soni <sscsps@gmail.com>
2018-01-26 16:55:44 +0530

    misc: fix a bit of indentation
    Signed-off-by: Sahil Soni <SscSPs@gmail.com>

--- device.mk
+++ device.mk
@@ -304,8 +304,9 @@ PRODUCT_PACKAGES += \
 
 # Vulkan
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
-    frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml
+	frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
+	frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml
+
 # Bluetooth
 PRODUCT_PACKAGES += \
 	libbt-vendor
