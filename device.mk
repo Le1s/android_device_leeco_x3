78d3bbb3e3c03ab91cae42525c01698d83c57646
vishalk17 <vishalkapadi95@gmail.com>
2018-09-02 00:36:10 +0530

    BLUETOOTH: Build Bluetooth from source

--- device.mk
+++ device.mk
@@ -295,3 +295,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
 	frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+	libbt-vendor \
+	libbluetooth_mtk
